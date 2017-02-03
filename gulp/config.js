cde3b379de29d3448ae5db8e35ae7447e84ddca3
Christophe (pc maison) <cavo789@yahoo.com>
2017-02-03 18:53:12 +0100

    Gulpifying

--- gulp/config.js
+++ gulp/config.js
@@ -1,118 +1,101 @@
 'use strict';
 
-var abspath  = 'c:/christophe/repository/markdown/';
-var source   = 'src';
-var target   = 'dist';
-var archives = 'backups';
-var log      = 'logs';
-
 /** 
   Read the package.json file of the project and return an object with the JSON representation  
 */
-function getSettings($node) {
+function getPackage() {
    var fs = require('fs');
    var json = JSON.parse(fs.readFileSync('package.json'));
    return json;
 }
 
+var path = require("path");
+
+var pkg      = getPackage();
+var paths    = pkg.gulp.paths;   // shortcut to the gulp.paths node of the package.json file
+
 module.exports = {
-   banner: [
-      '<?php',   
-      '/**',
-      '* <%= pkg().name %> - <%= pkg().description %>',
-      '* @version   : <%= pkg().version %>',
-      '* @author    : <%= pkg().author %>',
-      '* @copyright : <%= pkg().license %> (c) 2016 - <%= new Date().getFullYear() %>',
-      '* @url       : <%= pkg().homepage %>',
-      '* @package   : <%= new Date().toISOString() %>', 
-      '*/',
-      '?>', 
-      ''].join('\n'),
-   zip: {
-      src: [source + '/**/*'],
-	  dest: archives,
-	  name: 'Markdown'
+   backup: {
+      src: [paths.source + '/**/*'],
+	  dest: paths.archives
+   },  
+   SRC_phpcs: {
+      src: paths.source+'/',  
+	  exclude: '*/'+paths.exclude+'/*', // Process every php files except the libs folder  
+	  log: path.join(__dirname+'/../'+paths.logging+'/php-code-sniffer.xml') // Make an absolute filename
    },
-   phpcbf: {
-      doit: true,
-	  exclude: '*/libs/*', // Process every php files except the libs folder
-      src: abspath+source
-   },   
-   phplint: {
-      doit: true,
-      src: [source + '/**/*.php'],
+   SRC_phpmd: {
+      src: paths.source,    
+	  exclude: '*\\'+paths.exclude+'\\*', // Process every php files of the target folder except the libs folder
+	  log: path.join(__dirname+'/../'+paths.logging+'/php-mess_detector.xml') // Make an absolute filename
+   },
+   SRC_phplint: {
+      src: [paths.source + '/**/*.php','!' + paths.source + '/'+paths.exclude+'{,/**}'],  // Exclude libs folder
       options: {debug:false, clear:true, skipPassedFiles:true}
    },
-   jslint: {
-      doit: true, 
-      src: [source + '/**/*.js',
-         '!' + source + '/libs{,/**}'],
+   SRC_jslint: {
+      src: [paths.source + '/**/*.js','!' + paths.source + '/'+paths.exclude+'{,/**}'],  // Exclude libs folder
+      options:{}		 
+   },   
+   SRC_csslint: {
+      src: [paths.source + '/**/*.css','!' + paths.source + '/'+paths.exclude+'{,/**}'],  // Exclude libs folder
       options:{}		 
    },
-   
-   
-   
-   delete: {
-      dest: [target]
+   SRC_jsonlint: {
+      src: [paths.source + '/**/*.json','!' + paths.source + '/'+paths.exclude+'{,/**}'],  // Exclude libs folder
+      options:{}		 
+   },   
+   SRC_phpcbf: {
+	  extension: 'php',
+	  exclude: '*/'+paths.exclude+'/*', // Process every php files except the libs folder
+      src: paths.source //abspath+paths.source
+   },
+   SRC_jscbf: {
+	  extension: 'js',
+	  exclude: '*/'+paths.exclude+'/*', // Process every js files except the libs folder
+      src: paths.source //abspath+paths.source
+   },  
+   SRC_csscbf: {
+      src : [paths.source + '/**/*.css','!' + paths.source + '/'+paths.exclude+'{,/**}'],  // Exclude libs folder,
+      dest: paths.source
+   },  
+   SRC_jsoncbf: {
+      src:  [paths.source + '/**/*.json','!' + paths.source + '/'+paths.exclude+'{,/**}'],  // Exclude libs folder,
+	  dest: paths.source,
+      options:{}		 
+   },
+   DIST_delete: {
+      dest: [paths.target]
    }, 
-   copy: {
-      src: [source + '/**/*',                     // Copy all files under the _SRC folder but 
-         source + '/**/.htaccess',                //    included .htaccess (needed because "/*" ignore files without name (only extension))
-         '!' + source + '/libs/php_error{,/**}',  // === Don't copy these folders
-         '!' + source + '/libs/**/*.md',          //     No need to take .md files from /libs and subfolders
-         '!' + source + '/logs{,/**}',
-         '!' + source + '/nbproject{,/**}',
-         '!' + source + '/vendor{,/**}',
-         '!' + source + '/bower.json',            // === Don't copy these files
-         '!' + source + '/custom.css',
-         '!' + source + '/custom.js',
-         '!' + source + '/gulpfile.js',
-         '!' + source + '/post-composer.bat',
-         '!' + source + '/settings.json'
+   DIST_copy: {
+      src: [paths.source + '/**/*',                     // Copy all files under the _SRC folder but 
+         paths.source + '/**/.htaccess',                //    included .htaccess (needed because "/*" ignore files without name (only extension))
+         '!' + paths.source + '/libs/php_error{,/**}',  // === Don't copy these folders
+         '!' + paths.source + '/libs/**/*.md',          //     No need to take .md files from /libs and subfolders
+         '!' + paths.source + '/logs{,/**}',
+         '!' + paths.source + '/nbproject{,/**}',
+         '!' + paths.source + '/vendor{,/**}',
+         '!' + paths.source + '/bower.json',            // === Don't copy these files
+         '!' + paths.source + '/custom.css',
+         '!' + paths.source + '/custom.js',
+         '!' + paths.source + '/gulpfile.js',
+         '!' + paths.source + '/post-composer.bat',
+         '!' + paths.source + '/settings.json'
       ],
-      dest: target
+      dest: paths.target
    },
-   css: {
-      doit: true, 
-      src: [target + '/**/*.css',
-         '!' + target + '/libs{,/**}'             // === Ignore libs folde
-      ],
-      lint: false,
-      minify:true,
-      options: {},
-      dest: target
+   DIST_css: {
+      src: [paths.target + '/**/*.css','!' + paths.target + '/libs{,/**}'],  // Ignore the libs folder
+      dest: paths.target
    },
-   json: {
-      doit: true, 
-      src: [target + '/**/*.json',
-      '!' + target + '/libs{,/**}'],
-      lint: false,
-      dest: target
+   DIST_js: {
+      src: [paths.target + '/**/*.js','!' + paths.target + '/libs{,/**}'],  // Ignore the libs folder 
+      dest: paths.target
    },
-   js: {
-      doit: true, 
-      src: [target + '/**/*.js',
-      '!' + target + '/libs{,/**}'],
-      minify:true,
-      dest: target
-   },
-   php: {
+   DIST_php: {
       doit: true,
-      minify: true,
-      src: [target + '/**/*.php'],	
-      dest: target
-   },
-   phpmd: {
-      doit: true,
-      src: target,    
-	  exclude: '*\libs\*', // Process every php files of the target folder except the libs folder
-	  log: log
-   },
-   phpcs: {
-      doit: true,
-      src: abspath+target+'/',    
-	  log: abspath+log+'/php-code-sniffer.log'
-   },
-  
-   Settings:getSettings
+      src: [paths.target + '/**/*.php','!' + paths.target + '/libs{,/**}'],  // Ignore the libs folder 
+      dest: paths.target
+   },  
+   pkg:getPackage
 };
