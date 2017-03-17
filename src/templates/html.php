5b4c78c45a97011cfff242e81090a14ddfedb102
Christophe (pc maison) <cavo789@yahoo.com>
2017-03-17 21:03:35 +0100

    Template html

--- src/templates/html.php
+++ src/templates/html.php
@@ -8,8 +8,8 @@
 		<meta name="robots" content="%ROBOTS%" />
 		<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
 		<meta http-equiv="X-UA-Compatible" content="IE=9; IE=8;" />
-		<meta name="author" content="JUG Wallonie" />
-		<meta name="designer" content="JUG Wallonie" />
+		<meta name="author" content="Markdown | Notes management" />
+		<meta name="designer" content="Markdown | Notes management" />
 		<meta name="keywords" content="%TITLE%" />
 		<meta name="description" content="%TITLE%" />
 		<meta name="apple-mobile-web-app-capable" content="yes" />
