4c485fbb6e618ee19adbf9b62e3e0782f485389f
Christophe Avonture <cavo789@yahoo.com>
2017-03-29 14:34:14 +0200

    Disabling /dist temporarily

--- dist/assets/js/markdown_html.js
+++
@@ -1,9 +0,0 @@
-/**
-* markdown - Script that will transform your notes taken in the Markdown format (.md files) into a rich website
-* @version   : 1.0.5
-* @author    : christophe@aesecure.com
-* @license   : MIT
-* @url       : https://github.com/cavo789/markdown
-* @package   : 2017-03-27T08:34:06.625Z
-*/
-function addTOC(){if($("article h2").length>0){var e,a,i,n,t="<nav role='navigation' class='table-of-contents hidden-xs hidden-sm'><h2>Sur cette page:</h2><ul>";$("article h2").each(function(){a=$(this),i=a.text(),n="#"+a.attr("id"),e="<li><a href='"+n+"'>"+i+"</a></li>",t+=e}),t+="</ul></nav>",$("article").prepend(t)}}$("document").ready(function(){$("img").addClass("fullimg hidden-xs hidden-sm"),addTOC()});
---
+++ dist/readme.md
@@ -0,0 +1,3 @@
+The /dist folder has been disabled at that time because the number of improvements is high for this moment and it was not efficient at all to generated, at each release, a dist package.
+
+So, please, just use the files from the /src folder
