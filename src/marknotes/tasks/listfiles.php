56b11dd24de8f21ab79002f0c4aada84bb41f24e
Christophe Avonture <cavo789@yahoo.com>
2017-04-10 14:03:58 +0200

    rawurlencode

--- src/marknotes/tasks/listfiles.php
+++ src/marknotes/tasks/listfiles.php
@@ -10,7 +10,6 @@ defined('_MARKNOTES') or die('No direct access allowed');
 
 class ListFiles
 {
-
     protected static $_instance = null;
 
     public function __construct()
@@ -20,7 +19,6 @@ class ListFiles
 
     public static function getInstance()
     {
-
         if (self::$_instance === null) {
             self::$_instance = new ListFiles();
         }
@@ -30,46 +28,45 @@ class ListFiles
 
     public static function run()
     {
-
         $aeFiles = \MarkNotes\Files::getInstance();
         $aeFunctions = \MarkNotes\Functions::getInstance();
         $aeSettings = \MarkNotes\Settings::getInstance();
 
-        $sReturn='';
+        $sReturn = '';
 
         if ($aeSettings->getOptimisationUseServerSession()) {
             // Get the list of files/folders from the session object if possible
             $aeSession = \MarkNotes\Session::getInstance();
-            $sReturn=$aeSession->get('ListFiles', '');
+            $sReturn = $aeSession->get('ListFiles', '');
         }
 
-        if ($sReturn==='') {
-            $i=0;
+        if ($sReturn === '') {
+            $i = 0;
 
-            $arrFiles=$aeFunctions->array_iunique($aeFiles->rglob('*.md', $aeSettings->getFolderDocs(true)));
+            $arrFiles = $aeFunctions->array_iunique($aeFiles->rglob('*.md', $aeSettings->getFolderDocs(true)));
 
             // Be carefull, folders / filenames perhaps contains accentuated characters
-            $arrFiles=array_map('utf8_encode', $arrFiles);
+            $arrFiles = array_map('utf8_encode', $arrFiles);
 
             // Sort, case insensitve
             natcasesort($arrFiles);
 
-            $return['settings']['root']=$aeSettings->getFolderDocs(true);
+            $return['settings']['root'] = $aeSettings->getFolderDocs(true);
 
             // Get the number of files
-            $return['count']=count($arrFiles);
+            $return['count'] = count($arrFiles);
 
             // --------------------------------------------------------------------------------------
             // Populate the tree that will be used for jsTree (see https://www.jstree.com/docs/json/)
 
-            $folder=str_replace('/', DS, $aeSettings->getFolderDocs(true));
+            $folder = str_replace('/', DS, $aeSettings->getFolderDocs(true));
 
             // $arr is an array with arrays that contains arrays ...
             // i.e. the root folder that contains subfolders and subfolders can contains subfolders...
-            $arr=self::dir_to_jstree_array($folder, 'a', array('md'), $aeSettings->getTreeAutoOpen());
+            $arr = self::dir_to_jstree_array($folder, 'a', array('md'), $aeSettings->getTreeAutoOpen());
 
             // The array is now ready
-            $return['tree']=$arr;
+            $return['tree'] = $arr;
 
             $sReturn = json_encode($return, JSON_PRETTY_PRINT);
 
@@ -100,35 +97,33 @@ class ListFiles
         array $ext = array(),
         array $arrTreeAutoOpen
     ) : array {
-
-
-        $aeSettings=\MarkNotes\Settings::getInstance();
-        $root=str_replace('/', DS, $aeSettings->getFolderDocs(true));
-        $rootNode=$aeSettings->getFolderDocs(false);
+        $aeSettings = \MarkNotes\Settings::getInstance();
+        $root = str_replace('/', DS, $aeSettings->getFolderDocs(true));
+        $rootNode = $aeSettings->getFolderDocs(false);
 
         if (empty($ext)) {
-            $ext = array ("md");
+            $ext = array("md");
         }
 
         // The first note, root node, will always be opened (first level)
         // As from the second level, pay attention to the _settingsTreeOpened flag.  If not set, nodes will be
         // set in a closed state (user will need to click on the node to see items)
-        $opened=($root==$dir?1:$aeSettings->getTreeOpened());
+        $opened = ($root == $dir?1:$aeSettings->getTreeOpened());
 
         // if $this->_settingsFoldersAutoOpen if not empty, check if that folder is currently processing
 
-        if ($opened==false) {
+        if ($opened == false) {
             if (in_array(rtrim(utf8_encode($dir), DS), $arrTreeAutoOpen)) {
-                $opened=true;
+                $opened = true;
             }
         }
         // Entry for the folder
         $listDir = array(
-         'id'=>utf8_encode(str_replace($root, '', $dir).DS),
-         'type'=>'folder',
-         'icon'=>'folder',
-         'text' =>basename(utf8_encode($dir)),
-         'state'=>array('opened'=>$opened,'disabled'=>1),
+         'id' => utf8_encode(str_replace($root, '', $dir).DS),
+         'type' => 'folder',
+         'icon' => 'folder',
+         'text' => basename(rawurlencode($dir)),
+         'state' => array('opened' => $opened,'disabled' => 1),
          'children' => array());
 
         $files = array();
@@ -138,24 +133,24 @@ class ListFiles
             while (($sub = readdir($handler)) !== false) {
                 if ($sub != "." && $sub != "..") {
                     // Don't take files/folders starting with a dot
-                    if (substr($sub, 0, 1)!=='.') {
+                    if (substr($sub, 0, 1) !== '.') {
                         if (is_file($dir.DS.$sub)) {
                             $extension = pathinfo($dir.DS.$sub, PATHINFO_EXTENSION);
                             if (in_array($extension, $ext)) {
-                                $files[]=array(
-                                  'id'=>md5(utf8_encode(str_replace($root, $rootNode, $dir.DS.$sub))),
-                                  'icon'=>'file file-md',
-                                  'text'=>str_replace('.'.$extension, '', utf8_encode($sub)), // Don't display the extension
+                                $files[] = array(
+                                  'id' => md5(utf8_encode(str_replace($root, $rootNode, $dir.DS.$sub))),
+                                  'icon' => 'file file-md',
+                                  'text' => str_replace('.'.$extension, '', utf8_encode($sub)), // Don't display the extension
 
                                   // Populate the data attribute with the task to fire and the filename of the note
-                                  'data'=>array(
-                                 'task'=>'display',
-                                 'file'=>utf8_encode(str_replace($root, '', $dir.DS.$sub))
+                                  'data' => array(
+                                 'task' => 'display',
+                                 'file' => utf8_encode(str_replace($root, '', $dir.DS.$sub))
                                    )
                                 );
                             }
                         } elseif (is_dir($dir.DS.$sub)) {
-                            $dirs []= rtrim($dir, DS).DS.$sub;
+                            $dirs [] = rtrim($dir, DS).DS.$sub;
                         }
                     } // if (substr($sub, 0, 1)!=='.')
                 }
@@ -168,7 +163,7 @@ class ListFiles
             }
 
             foreach ($dirs as $d) {
-                $listDir['children'][]=self::dir_to_jstree_array($d, $order, $ext, $arrTreeAutoOpen);
+                $listDir['children'][] = self::dir_to_jstree_array($d, $order, $ext, $arrTreeAutoOpen);
             }
 
             if ($order === "a") {
@@ -178,7 +173,7 @@ class ListFiles
             }
 
             foreach ($files as $file) {
-                $listDir['children'][]= $file;
+                $listDir['children'][] = $file;
             }
 
             closedir($handler);
--- src/router.php
+++ src/router.php
@@ -19,94 +19,94 @@ use \MarkNotes\Autoload;
 \MarkNotes\Autoload::register();
 
 /*<!-- build:debug -->*/
-$aeDebug=\MarkNotes\Debug::getInstance();
+$aeDebug = \MarkNotes\Debug::getInstance();
 /*<!-- endbuild -->*/
 
 // Application root folder.
-$folder=str_replace('/', DS, dirname($_SERVER['SCRIPT_FILENAME']));
-$folder=rtrim($folder, DS).DS;
+$folder = str_replace('/', DS, dirname($_SERVER['SCRIPT_FILENAME']));
+$folder = rtrim($folder, DS).DS;
 
 $aeFiles = \MarkNotes\Files::getInstance();
 $aeFunctions = \MarkNotes\Functions::getInstance();
 
-$filename=utf8_decode($aeFunctions->getParam('file', 'string', '', false));
+$filename = rawurldecode($aeFunctions->getParam('file', 'string', '', false));
 
-$params=array('filename'=>$filename);
+$params = array('filename' => $filename);
 $aeSettings = \MarkNotes\Settings::getInstance($folder, $params);
 
 // Retrieve the asked extension i.e. if the user try to access the /note.html or /note.pdf file,
 // extract the extension (html or pdf)
-$format='';
-if ($filename!=='') {
-    $format=$aeFiles->getExtension($filename);
+$format = '';
+if ($filename !== '') {
+    $format = $aeFiles->getExtension($filename);
 }
 
 // Take a look on the format parameter, if mentionned use that format
-$format=$aeFunctions->getParam('format', 'string', $format, false, 8);
+$format = $aeFunctions->getParam('format', 'string', $format, false, 8);
 
 // Only these format are recognized.  Default : html
 if (!(in_array($format, array('htm','html','pdf','slides')))) {
-    $format='html';
+    $format = 'html';
 }
-$params=array();
+$params = array();
 
-if ($filename!=='') {
-    $fileMD='';
+if ($filename !== '') {
+    $fileMD = '';
 
     if (in_array($filename, array('timeline.html', 'sitemap.xml'))) {
         // Specific files
 
         switch ($filename) {
             case 'timeline.html':
-                $task='timeline';
+                $task = 'timeline';
                 break;
 
             case 'sitemap.xml':
-                $task='sitemap';
+                $task = 'sitemap';
                 break;
         } // switch
 
-        $filename='';
+        $filename = '';
     } else {
-        $layout=$aeFunctions->getParam('layout', 'string', '', false, 10);
+        $layout = $aeFunctions->getParam('layout', 'string', '', false, 10);
         if (!(in_array($layout, array('remark','reveal')))) {
-            $layout='';
+            $layout = '';
         }
 
-        if ($layout!=='') {
-            $params['layout']=$layout;
+        if ($layout !== '') {
+            $params['layout'] = $layout;
         }
 
         switch ($format) {
             case 'pdf':
-                $task='pdf';
+                $task = 'pdf';
                 break;
 
             case 'slides':
                 // Check on the URL if the user has forced a layout i.e. "remark" or "reveal", the supported slideshow framework
 
-                $task='slideshow';
+                $task = 'slideshow';
 
                 break;
 
             default:                  // htm or html
-                $task='display';
+                $task = 'display';
                 if (!isset($params['layout'])) {
-                    $params['layout']='html';
+                    $params['layout'] = 'html';
                 }
                 break;
         } // switch
 
         // Get the absolute folder name where the web application resides (f.i. c:\websites\marknotes\)
-        $webRoot=$aeSettings->getFolderWebRoot(true);
+        $webRoot = $aeSettings->getFolderWebRoot(true);
 
         // Build the full filename
-        $filename=str_replace('/', DIRECTORY_SEPARATOR, $filename);
+        $filename = str_replace('/', DIRECTORY_SEPARATOR, $filename);
 
-        $fileMD=$aeFiles->removeExtension($filename).'.md';
+        $fileMD = $aeFiles->removeExtension($filename).'.md';
 
         if (!$aeFiles->fileExists($webRoot.$fileMD)) {
-            $fileMD=utf8_decode($fileMD);
+            $fileMD = utf8_decode($fileMD);
         }
 
         if (!$aeFiles->fileExists($webRoot.$fileMD)) {
