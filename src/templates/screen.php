c12143a11d9982b722c4f68e79447cf35b5eed8f
Christophe Avonture <cavo789@yahoo.com>
2017-04-11 13:45:18 +0200

    favicon - once more

--- src/templates/screen.php
+++ src/templates/screen.php
@@ -40,6 +40,11 @@
 		<link rel="apple-touch-icon" sizes="152x152" href="assets/images/favicons/apple-touch-icon-152x152.png">
 		<link rel="apple-touch-icon" sizes="180x180" href="assets/images/favicons/apple-touch-icon-180x180.png">
 		<link rel="icon" type="image/png" href="assets/images/favicons/favicon-32x32.png" sizes="32x32">
+		<link rel="icon" type="image/png" href="assets/images/favicons/android-icon-36x36.png" sizes="36x36">
+		<link rel="icon" type="image/png" href="assets/images/favicons/android-icon-48x48.png" sizes="48x48">
+		<link rel="icon" type="image/png" href="assets/images/favicons/android-icon-72x72.png" sizes="72x72">
+		<link rel="icon" type="image/png" href="assets/images/favicons/android-icon-96x96.png" sizes="96x96">
+		<link rel="icon" type="image/png" href="assets/images/favicons/android-icon-144x144.png" sizes="144x144">
 		<link rel="icon" type="image/png" href="assets/images/favicons/android-icon-192x192.png" sizes="192x192">
 		<link rel="icon" type="image/png" href="assets/images/favicons/favicon-16x16.png" sizes="16x16">
 		<link rel="manifest" href="assets/images/favicons/manifest.json">
