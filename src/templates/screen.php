5ed615d62992cf191e6860188bd514f05a2e82cb
Christophe Avonture <cavo789@yahoo.com>
2017-04-11 13:39:46 +0200

    favicons

--- src/templates/screen.php
+++ src/templates/screen.php
@@ -30,11 +30,12 @@
 
         <title>%APP_NAME%</title>
 
-        <link rel="apple-touch-icon" sizes="180x180" href="assets/images/favicons/apple-touch-icon.png">
-        <link rel="icon" type="image/png" href="assets/images/favicons/favicon-32x32.png" sizes="32x32">
-        <link rel="icon" type="image/png" href="assets/images/favicons/favicon-16x16.png" sizes="16x16">
-        <link rel="manifest" href="assets/images/favicons/manifest.json">
-        <meta name="theme-color" content="#ffffff">
+		<link rel="apple-touch-icon" sizes="180x180" href="assets/images/favicons/apple-touch-icon.png">
+		<link rel="icon" type="image/png" href="assets/images/favicons/favicon-32x32.png" sizes="32x32">
+		<link rel="icon" type="image/png" href="assets/images/favicons/favicon-16x16.png" sizes="16x16">
+		<link rel="manifest" href="assets/images/favicons/manifest.json">
+		<link rel="mask-icon" href="assets/images/favicons/safari-pinned-tab.svg" color="#5bbad5">
+		<meta name="theme-color" content="#ffffff">
 
         <!--%FONT%-->
 
