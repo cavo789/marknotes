c50ee77467fa7a213dc911be34b1022a486cc2ff
Christophe <cavo789@yahoo.com>
2017-12-18 22:46:14 +0100

    Update task

--- src/marknotes/plugins/page/html/update/update.js
+++ src/marknotes/plugins/page/html/update/update.js
@@ -4,9 +4,8 @@ function fnPluginHTMLUpdate() {
 	// Idea is to detect the current version and the version on
 	// github and to change the color of the icon
 	// so the user knows he can start an update
-	$('#icon_update').css('color','#ff8c4e');
-
-	document.getElementById('icon_update').title = $.i18n('update_newer');
+	//$('#icon_update').css('color','#ff8c4e');
+	//document.getElementById('icon_update').title = $.i18n('update_newer');
 }
 
 /**
@@ -20,11 +19,43 @@ function fnPluginTaskUpdate() {
 	}
 	/*<!-- endbuild -->*/
 
-	$.post(
-		"index.php", {
-			"task": "task.update.run"
+	// Redirect to the install script
+	Noty({
+		message: $.i18n('update_redirect'),
+		type: 'info'
+	});
+
+	try {
+		ajaxify({
+			task: 'task.update.run',
+			callback: 'fnPluginTaskUpdateDoIt($data, data);'
+		});
+	} catch (err) {
+		console.warn(err.message);
+	}
+
+	return true;
+}
+
+/**
+ * $data is the parameters sent with the ajax request
+ * data is the answer of the task.update.run call
+ */
+function fnPluginTaskUpdateDoIt($data, data) {
+
+	/*<!-- build:debug -->*/
+	if (marknotes.settings.debug) {
+		console.log('In fnPluginTaskUpdateDoIt function');
+	}
+	/*<!-- endbuild -->*/
+
+	if (data.hasOwnProperty('status')) {
+		if (parseInt(data.status) === 1) {
+			location.href = 'install.php';
 		}
-	);
+
+	}
+
 
 	return true;
 }
--- src/marknotes/plugins/task/update/run.php
+++ src/marknotes/plugins/task/update/run.php
@@ -1,6 +1,11 @@
 <?php
 /**
  * Update marknotes
+ *
+ * Retrieve the install.php script from https://raw.githubusercontent.com/cavo789/marknotes_install/master/install.php
+ *
+ * The url is specified in settings.json in the
+ * plugins->options->task->update->url entry
  */
 namespace MarkNotes\Plugins\Task\Update;
 
@@ -14,11 +19,51 @@ class Update extends \MarkNotes\Plugins\Task\Plugin
 
 	public static function run(&$params = null) : bool
 	{
+		$aeSettings = \MarkNotes\Settings::getInstance();
+		$root = $aeSettings->getFolderWebRoot();
 
-/*<!-- build:debug -->*/
-$aeDebug = \MarkNotes\Debug::getInstance();
-$aeDebug->here("", 10);
-/*<!-- endbuild -->*/
+		// Get the URL to the update script
+		// i.e. https://raw.githubusercontent.com/cavo789/marknotes_install/master/install.php
+		$url = trim(self::getOptions('url', ''));
+
+		$bStatus = 0;
+
+		if ($url !== '') {
+			// Use GuzzleHttp
+			$client = new \GuzzleHttp\Client(
+				array('curl'=>array(CURLOPT_SSL_VERIFYPEER=>false))
+			);
+
+			$res = $client->request('GET', $url,
+				['connect_timeout' => 10]);
+
+			if ($res->getStatusCode() == 200) {
+				// Get the script content; it's the PHP source of
+				// marknotes_install
+				$content = $res->getBody();
+
+				// Get the root folder of the website
+				$root = rtrim($aeSettings->getFolderWebRoot(), DS).DS;
+
+				/*<!-- build:debug -->*/
+				if ($aeSettings->getDebugMode()) {
+					$aeDebug = \MarkNotes\Debug::getInstance();
+					$aeDebug->log("Create ".$root."install.php", "debug");
+				}
+				/*<!-- endbuild -->*/
+
+				$aeFiles = \MarkNotes\Files::getInstance();
+				$aeFiles->fwriteUTF8BOM($root.'install.php', $content);
+
+				// True if the file has been created
+				$bStatus = (is_file($root.'install.php'));
+
+			} // if ($res->getStatusCode() == 200)
+		} // if ($url !== '')
+
+		header('Content-Type: application/json');
+		echo json_encode(array('status' => ($bStatus?1:0)));
+
 		return true;
 	}
 }
